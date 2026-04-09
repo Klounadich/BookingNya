31fe48015ad78e0162845e7fc288ddc07f9c1348
Klounadich <kekers507y@gmail.com>
2026-04-09 15:56:23 +0300

    v1.0.0-alpha

--- InventoryModule/obj/Debug/net10.0/InventoryModule.AssemblyInfo.cs
+++ InventoryModule/obj/Debug/net10.0/InventoryModule.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryModule")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ceaa548e7a92908e42c0e82902e23d98e2789a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+696cd87a0f139f2dca450dc8b904a7073f0a54e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryModule")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryModule")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
